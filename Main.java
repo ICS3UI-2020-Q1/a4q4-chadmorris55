e37796bfdbcbfa9b9b3cc3ec65022a62666a5d09
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-02 20:17:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * finds the largest integer out of the three given
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
+    //store constants here
+    final int FIZZ_DIV = 3;
+    final int BUZZ_DIV = 5;
+
+    System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
+    int number = input.nextInt();
+    int count = 1;
+    while (count <= number){ 
+      if(count % FIZZ_DIV == 0 && count % BUZZ_DIV == 0){
+        System.out.println("FizzBuzz");
+        count = count + 1;
+      } else if (count % FIZZ_DIV == 0){
+        System.out.println("Fizz");
+        count = count + 1;
+      } else if (count % BUZZ_DIV == 0){
+        System.out.println("Buzz");
+        count = count + 1;
+      } else {
+        System.out.println(count);
+        count = count + 1;
+      }
+
+    }
   }
 }
